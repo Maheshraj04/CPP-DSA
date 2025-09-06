ba03e338072d2e1a7c97b2b2966594e7b1a8fe03
Mahesh Raj <maheshraj6761@gmail.com>
2025-09-06 13:47:47 +0530

    fibonacci by recursion

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,21 @@
+// fibonaci series by recursion
+#include<iostream>
+using namespace std;
+int fibo(int n){
+    // base case
+    if(n==0  )
+    return 0;
+    if(n==1)
+    return 1;
+
+    int ans = fibo(n-1) +fibo(n-2);
+
+return ans;
+}
+
+int main(){
+int n ;
+cin>> n;
+cout<< "The fibo of number is:"<<fibo(n)<<endl;
+return 0;
+}
